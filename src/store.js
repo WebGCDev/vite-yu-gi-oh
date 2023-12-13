f1dc72f37a0c6e4b12bafc0c8a6e07f0059f2902
WebGCDev <gaetano.chi@gmail.com>
2023-12-13 16:52:08 +0100

    feat: Loading All Archetype & Found 12899 cards

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue'
 import axios from 'axios'
 export const store = reactive({
     loading: true,
-    YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
     ARCH_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?archetype=',
     selectArch: null,
     cards: null,
