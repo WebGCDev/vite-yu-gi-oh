69b858d10acc2ccc48f9f6f2945e1d15e3a8a450
WebGCDev <gaetano.chi@gmail.com>
2023-12-13 01:43:09 +0100

    feat: add Try Loading "Get all cards" 1.36 min test ok

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue'
 import axios from 'axios'
 export const store = reactive({
     loading: true,
-    YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=100&offset=0',
+    YUGI_API: 'https://db.ygoprodeck.com/api/v7/cardinfo.php',
     cards: null,
     fetchCard(url) {
         axios.get(url)
